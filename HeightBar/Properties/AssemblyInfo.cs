064dedfa6472633de868f7d1ab7c2320c575ee03
ManlyMarco <dummyaccx@gmail.com>
2019-10-15 22:21:41 +0200

    Set assembly version

--- HeightBar/Properties/AssemblyInfo.cs
+++ HeightBar/Properties/AssemblyInfo.cs
@@ -6,12 +6,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("3dc1f962-8955-4746-8dd0-5d814fd6a6a9")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion(HeightBar.HeightBar.Version)]
+[assembly: AssemblyVersion(HeightBar.HeightBar.Version)]
