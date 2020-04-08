0379ae2d5819e9564882d66564b4b2159fcf7957
ManlyMarco <dummyaccx@gmail.com>
2020-04-08 22:30:52 +0200

    Added EC version

---
+++ EC_HeightBar/HeightBar.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using KKAPI;
+
+namespace HeightBar
+{
+    [BepInProcess("EmotionCreators")]
+    [BepInDependency(KoikatuAPI.GUID, "1.8")]
+    public partial class HeightBar : BaseUnityPlugin
+    {
+        private readonly float Ratio = 103.092781f;
+    }
+}
---
+++ EC_HeightBar/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EC_HeightBar")]
+[assembly: AssemblyProduct("EC_HeightBar")]
+[assembly: AssemblyDescription("Shows a height measure bar in the character maker")]
